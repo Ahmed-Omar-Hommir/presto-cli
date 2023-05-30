50eddb64e117a7d42fdf0a5343546168fe95f659
Ahmed-Omar-Prestoeat <a.hommir@prestoeat.com>
2023-05-30 03:50:46 -0700

    fix: current dir in test

--- test/version_test.dart
+++ test/version_test.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:path/path.dart';
 import 'package:presto_cli/src/utils/utils.dart';
 import 'package:test/test.dart';
 import 'package:presto_cli/src/version.dart';
@@ -15,9 +12,7 @@ void main() {
     'should ensure that packageVersion in pubspec.yaml matches generated packageVersion.',
     () async {
       // Arrange
-      print(Directory.current.path);
-      final pubspecPath = join(Directory.current.path, 'pubspec.yaml');
-      final result = await fileManager.readYaml(pubspecPath);
+      final result = await fileManager.readYaml('pubspec.yaml');
 
       // Act
       final generatedVersion = packageVersion;
